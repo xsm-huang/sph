2b2ba2c06b4d77c461cfb480e698cc055cf4a70f
xsm-huang <1012973656@qq.com>
2022-04-10 21:39:10 +0800

    引入mock模拟数据

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import TypeNav from '@/components/TypeNav'; // 三级联动组件 注册全局组件
 import router from '@/router/index'; // 引入路由\
 import store from '@/store'; // 引入仓库
+import '@/mock/mockServe'; // 引入mock模拟数据
 
 Vue.config.productionTip = false;
 
