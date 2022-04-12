01a181202c46ac4c844024588f2b7208fef50b81
xsm-huang <1012973656@qq.com>
2022-04-12 17:00:12 +0800

    分页器绑定动态数据

--- src/components/pagination/index.vue
+++ src/components/pagination/index.vue
@@ -1,23 +1,81 @@
 <template>
     <div class="pagination">
-        <button>上一页</button>
-        <button>1</button>
-        <button>...</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>...</button>
-        <button>9</button>
-        <button>下一页</button>
-        <button class="total">共60条</button>
+        <button class="pgUp" :disabled="pageNo == 1" @click="$emit('getPageNo', pageNo - 1)">
+            上一页
+        </button>
+        <button
+            :class="{ active: pageNo == 1 }"
+            v-if="continuesNum[0] > 1"
+            @click="$emit('getPageNo', 1)"
+        >
+            1
+        </button>
+        <button v-if="continuesNum[0] > 2">...</button>
+        <button
+            :class="{ active: pageNo == page }"
+            v-for="(page, index) in continuesNum"
+            :key="index"
+            @click="$emit('getPageNo', page)"
+        >
+            {{ page }}
+        </button>
+        <button v-if="continuesNum[continuesNum.length - 1] < totalPage - 1">...</button>
+        <button
+            :class="{ active: pageNo == totalPage }"
+            v-if="continuesNum[continuesNum.length - 1] < totalPage"
+            @click="$emit('getPageNo', totalPage)"
+        >
+            {{ totalPage }}
+        </button>
+        <button
+            class="pgDn"
+            :disabled="pageNo == totalPage"
+            @click="$emit('getPageNo', pageNo + 1)"
+        >
+            下一页
+        </button>
+        <button class="total">共{{ total }}条</button>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Pagination',
+    props: ['pageNo', 'pageSize', 'total', 'continues'],
+    computed: {
+        // 总页数
+        totalPage() {
+            return Math.ceil(this.total / this.pageSize);
+        },
+
+        continuesNum() {
+            const { pageNo, totalPage, continues } = this;
+            let start = 0;
+            let end = 0;
+            if (continues >= totalPage) {
+                start = 1;
+                end = totalPage;
+            } else {
+                start = pageNo - Math.floor(continues / 2);
+                end = pageNo + Math.floor(continues / 2);
+                if (start < 1) {
+                    start = 1;
+                    end = continues;
+                }
+                if (end > totalPage) {
+                    start = totalPage - continues + 1;
+                    end = totalPage;
+                }
+            }
+
+            let continuesNum = [];
+            while (start <= end) {
+                continuesNum.push(start);
+                start++;
+            }
+            return continuesNum;
+        },
+    },
 };
 </script>
 
@@ -41,6 +99,15 @@ export default {
 
     .total {
         margin-left: 30px;
+    }
+
+    .active,
+    .pgUp:hover,
+    .pgDn:hover {
+        background-color: rgb(255, 131, 131);
+    }
+
+    .pgUp:hover {
     }
 }
 </style>
