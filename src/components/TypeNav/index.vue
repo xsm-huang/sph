93495967eef516858be4c448243a4339e980dadb
xsm-huang <1012973656@qq.com>
2022-04-12 20:13:58 +0800

    修改动画

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- 商品分类导航 -->
     <div class="type-nav">
-        <div class="container" @mouseenter="enterShow" @mouseleave="leaveHide">
-            <h2 class="all">全部商品分类</h2>
+        <div class="container" @mouseleave="leaveHide">
+            <h2 class="all" @mouseenter="enterShow">全部商品分类</h2>
             <nav class="nav">
                 <a href="###">服装城</a>
                 <a href="###">美妆馆</a>
